2af34577680b687ca99c862c41cb9c6f841d537c
mrmuffinnxz <71134901+mrmuffinnxz@users.noreply.github.com>
2022-03-28 23:29:23 +0700

    diamond_shape

---
+++ math-array-string/problem/daimond_shape.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    // for(int i=0; i<n; i++) {
+    //     if(i <= n/2) {
+    //         for(int j=0; j<n/2-i; j++) {
+    //             cout << " ";
+    //         }
+    //         for(int j=0;j<2*i+1;j++) {
+    //             cout << "+";
+    //         }
+    //     } else {
+    //         for(int j=0; j<i-n/2; j++) {
+    //             cout << " ";
+    //         }
+    //         for(int j=0;j<2*(n-i-1)+1;j++) {
+    //             cout << "+";
+    //         }
+    //     }
+    //     cout << "\n";
+    // }
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(i <= n/2) {
+               if(j >= n/2-i && j <= n/2+i) {
+                    cout << "+";
+                } else {
+                    cout << " ";
+                } 
+            } else {
+                if(j < (i-n/2) || j > (n+n/2-i-1)) {
+                    cout << " ";
+                } else {
+                    cout << "+";
+                }
+            }
+        }
+        cout << "\n";
+    }
+}
